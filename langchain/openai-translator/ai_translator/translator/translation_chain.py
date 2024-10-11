bec698a901b37acde11d6bba94b20cff10200847
jaredhshuai <jaredshuai@seismic.com>
2024-10-11 16:47:13 +0800

    基于 ChatGLM2-6B 实现带图形化界面的 openai-translator
    基于 ChatGLM2-6B 实现带图形化界面的 openai-translator

--- langchain/openai-translator/ai_translator/translator/translation_chain.py
+++ langchain/openai-translator/ai_translator/translator/translation_chain.py
@@ -1,5 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.llms import ChatGLM
 
 from utils import LOG
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
@@ -24,7 +25,14 @@ class TranslationChain:
         )
 
         # 为了翻译结果的稳定性，将 temperature 设置为 0
-        chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
+        #chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=verbose)
+        endpoint_url = "http://127.0.0.1:8000"
+        chat = ChatGLM(
+            endpoint_url=endpoint_url,
+            max_tokens=80000,
+            prefix_messages=messages,
+            top_p=0.9,
+        )
 
         self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
